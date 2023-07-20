a609d4f0e6256162476ad61c7ca91a83dd9d10c0
Rasel <103592581+alfred-rasel@users.noreply.github.com>
2023-07-20 17:26:44 +0600

    connect with my sql database

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT=3009;
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
-
-
+const dbService = require('./dbService');
 
 
 
@@ -15,13 +14,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
 //create 
+app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+    
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
+});
+
 //read 
-app.get('/getAll',(request,response)=>{
-    response.json({
-        success:true
-    });
+app.get('/getAll', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
 })
+
+
+  
 //update 
 //delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
 //run server
 app.listen(process.env.PORT, () => console.log('app is running'));
